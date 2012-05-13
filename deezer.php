1a56c6793bb214940042c12fbc5025a222fd3b09
Arnaud Costes <arnaud.costes@gmail.com>
2012-05-13 15:46:01 +0200

    directory lib not needed

--- deezer.php
+++ deezer.php
@@ -6,7 +6,6 @@ define('DEEZER_API_URL', 'http://api.deezer.com/' . DEEZER_API_VERSION);
 
 set_include_path(implode(PATH_SEPARATOR, array(
     DEEZER_API_DIR . '/data/models',
-    DEEZER_API_DIR . '/lib',
     get_include_path()
 )));
 
